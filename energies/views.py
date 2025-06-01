fc9f9a5ca908b336f39b69afdb822b1e8cd363eb
nnischal <nischalniroula18@gmail.com>
2025-06-01 21:23:54 +0545

    email notifications and alert message

--- energies/views.py
+++ energies/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import ContactForm
-
+from django.conf import settings
+from django.core.mail import send_mail
 def home(request):
     return render(request, 'energies/home.html')
 
@@ -16,7 +18,24 @@ def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            # Send email using Django's send_mail function
+            instance = form.save()
+            subject = f"New Contact Form Submission from {instance.name}"
+            message =f"""
+You have received a new message from the contact form.
+Name: {instance.name}
+Email: {instance.email}
+Contact:{instance.contact}
+Message: {instance.message}"""
+            send_mail(
+                subject,
+                message,
+                settings.DEFAULT_FROM_EMAIL,  
+                [settings.CONTACT_EMAIL],  
+                fail_silently=False,
+            )
             form.save()
+            messages.success(request, "Your message has been sent successfully!")
             return redirect('contact')  # You can redirect to a success page
     else:
         form = ContactForm()
--- samyati/settings.py
+++ samyati/settings.py
@@ -126,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'samyatielectrowheels@gmail.com'  # 🔁 Replace with your Gmail
+EMAIL_HOST_PASSWORD = 'iqgo octv mfaj ckmp'  # 🔁 Use an App Password (not your real Gmail password)
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+CONTACT_EMAIL = 'samyatielectrowheels@gmail.com'
